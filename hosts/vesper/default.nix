9798984a8914ac4e2887bec014e57c1f7921a1b4
Onur <51912619+OnurByte@users.noreply.github.com>
2026-08-17 01:22:48 +0300

    desktop: keep the full Vesper locale in English

--- hosts/vesper/default.nix
+++ hosts/vesper/default.nix
@@ -14,7 +14,24 @@
 
   networking.hostName = hostname;
   time.timeZone = "Europe/Istanbul";
-  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Keep the whole desktop/session language in English. Hyprland and Wayland
+  # applications inherit these locale categories; keyboard layout is separate.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+    };
+  };
 
   # Keep this at the version used for the first installation.
   system.stateVersion = "26.05";
