6b24dca5f531519808097a2d58ec028bcd711d6b
Onur <51912619+OnurByte@users.noreply.github.com>
2026-08-17 22:37:04 +0300

    network: build hardened proxy path

--- home/yargc/packages/vesper-control.nix
+++ home/yargc/packages/vesper-control.nix
@@ -31,6 +31,7 @@ stdenv.mkDerivation {
     cp ${./vesper-provider-registry.rs} vesper-provider-registry.rs
     patch vesper-control.rs < ${./vesper-control-provider-registry.patch}
     patch vesper-control.rs < ${./vesper-control-wifi-qr.patch}
+    patch vesper-control.rs < ${./vesper-control-proxy-hardening.patch}
     patch vesper-control.rs < ${./vesper-control-wellbeing.patch}
     patch vesper-control.rs < ${./vesper-control-credential-aliases.patch}
     patch vesper-control.rs < ${./vesper-control-exec-separator.patch}
