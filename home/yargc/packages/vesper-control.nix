b7f8491e397cbd6d38f00e09b2c9e569d40c56f0
Onur <51912619+OnurByte@users.noreply.github.com>
2026-08-18 04:29:26 +0300

    settings: route icon control through Rust worker

--- home/yargc/packages/vesper-control.nix
+++ home/yargc/packages/vesper-control.nix
@@ -31,9 +31,11 @@ stdenv.mkDerivation {
   buildPhase = ''
     runHook preBuild
     cp ${./vesper-control.rs} vesper-control.rs
+    cp ${./vesper-control-router.rs} vesper-control-router.rs
     cp ${./vesper-icons.rs} vesper-icons.rs
     patch vesper-control.rs < ${./vesper-control-wifi-qr.patch}
-    rustc --edition=2021 -C opt-level=2 vesper-control.rs -o vesper-control
+    rustc --edition=2021 -C opt-level=2 vesper-control.rs -o vesper-control-core
+    rustc --edition=2021 -C opt-level=2 vesper-control-router.rs -o vesper-control
     rustc --edition=2021 -C opt-level=2 vesper-icons.rs -o vesper-icon-engine
     runHook postBuild
   '';
@@ -41,6 +43,7 @@ stdenv.mkDerivation {
   installPhase = ''
     runHook preInstall
     install -Dm755 vesper-control $out/bin/vesper-control
+    install -Dm755 vesper-control-core $out/bin/vesper-control-core
     install -Dm755 vesper-icon-engine $out/bin/vesper-icon-engine
 
     runtimePath=${lib.makeBinPath [
@@ -59,6 +62,8 @@ stdenv.mkDerivation {
 
     wrapProgram $out/bin/vesper-control \
       --prefix PATH : "$out/bin:$runtimePath"
+    wrapProgram $out/bin/vesper-control-core \
+      --prefix PATH : "$runtimePath"
     wrapProgram $out/bin/vesper-icon-engine \
       --prefix PATH : "$runtimePath"
     runHook postInstall
