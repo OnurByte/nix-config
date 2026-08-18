e6b2bcb479afc7b80736fb9de1d90b4d0d32a5bd
Onur <51912619+OnurByte@users.noreply.github.com>
2026-08-18 14:50:50 +0300

    adaptive icons: wire persistent queue supervisor

--- home/yargc/packages/vesper-control.nix
+++ home/yargc/packages/vesper-control.nix
@@ -18,7 +18,7 @@
 }:
 stdenv.mkDerivation {
   pname = "vesper-control";
-  version = "0.2.0";
+  version = "0.3.0";
 
   dontUnpack = true;
 
@@ -33,10 +33,14 @@ stdenv.mkDerivation {
     cp ${./vesper-control.rs} vesper-control.rs
     cp ${./vesper-control-router.rs} vesper-control-router.rs
     cp ${./vesper-icons.rs} vesper-icons.rs
+    cp ${./vesper-icon-queue.rs} vesper-icon-queue.rs
+    cp ${./vesper-icon-supervisor.rs} vesper-icon-supervisor.rs
     patch vesper-control.rs < ${./vesper-control-wifi-qr.patch}
     rustc --edition=2021 -C opt-level=2 vesper-control.rs -o vesper-control-core
     rustc --edition=2021 -C opt-level=2 vesper-control-router.rs -o vesper-control
-    rustc --edition=2021 -C opt-level=2 vesper-icons.rs -o vesper-icon-engine
+    rustc --edition=2021 -C opt-level=2 vesper-icons.rs -o vesper-icon-engine-core
+    rustc --edition=2021 -C opt-level=2 vesper-icon-queue.rs -o vesper-icon-queue
+    rustc --edition=2021 -C opt-level=2 vesper-icon-supervisor.rs -o vesper-icon-engine
     runHook postBuild
   '';
 
@@ -45,6 +49,8 @@ stdenv.mkDerivation {
     install -Dm755 vesper-control $out/bin/vesper-control
     install -Dm755 vesper-control-core $out/bin/vesper-control-core
     install -Dm755 vesper-icon-engine $out/bin/vesper-icon-engine
+    install -Dm755 vesper-icon-engine-core $out/bin/vesper-icon-engine-core
+    install -Dm755 vesper-icon-queue $out/bin/vesper-icon-queue
 
     runtimePath=${lib.makeBinPath [
       bluez
@@ -65,6 +71,10 @@ stdenv.mkDerivation {
     wrapProgram $out/bin/vesper-control-core \
       --prefix PATH : "$runtimePath"
     wrapProgram $out/bin/vesper-icon-engine \
+      --prefix PATH : "$out/bin:$runtimePath"
+    wrapProgram $out/bin/vesper-icon-engine-core \
+      --prefix PATH : "$runtimePath"
+    wrapProgram $out/bin/vesper-icon-queue \
       --prefix PATH : "$runtimePath"
     runHook postInstall
   '';
