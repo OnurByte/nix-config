e0b7a06d5794e5d64a5741e7c5ef5252324ae1aa
Onur <51912619+OnurByte@users.noreply.github.com>
2026-08-18 15:48:23 +0300

    adaptive icons: apply versioned grid recipe

--- home/yargc/packages/vesper-control.nix
+++ home/yargc/packages/vesper-control.nix
@@ -34,6 +34,7 @@ let
       patch vesper-icons.rs < ${./vesper-icons-material-axis.patch}
       patch vesper-icons.rs < ${./vesper-icons-format-support.patch}
       patch vesper-icons.rs < ${./vesper-icons-appstream-recovery.patch}
+      patch vesper-icons.rs < ${./vesper-icons-grid-recipe.patch}
       patch vesper-icon-worker.rs < ${./vesper-icon-worker-provider-defaults.patch}
     '';
     doCheck = false;
